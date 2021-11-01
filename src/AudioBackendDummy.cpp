92a0549f2e45b18bbc24f5c9e1007e15ac8fdf2a
dqpb <dqpb@mailbox.org>
2021-11-01 19:07:39 +0100

    Add dummy audio backend

---
+++ src/AudioBackendDummy.cpp
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2021 The GMetronome Team
+ * 
+ * This file is part of GMetronome.
+ *
+ * GMetronome is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GMetronome is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GMetronome.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "AudioBackendDummy.h"
+#include <thread>
+
+namespace audio {
+
+  DummyBackend::DummyBackend(const audio::SampleSpec& spec)
+    : spec_(spec)
+  {}
+
+  void DummyBackend::configure(const SampleSpec& spec)
+  {
+    spec_ = spec;
+  }
+
+  void DummyBackend::open()
+  {
+    if (state_ == BackendState::kConfig)
+      state_ = BackendState::kOpen;
+    else
+      throw BackendStateTransitionError();
+  }
+
+  void DummyBackend::close()
+  {
+    if (state_ == BackendState::kOpen)
+      state_ = BackendState::kConfig;
+    else
+      throw BackendStateTransitionError();
+  }
+
+  void DummyBackend::start()
+  {
+    if (state_ == BackendState::kOpen)
+      state_ = BackendState::kRunning;
+    else
+      throw BackendStateTransitionError();
+  }
+
+  void DummyBackend::stop()
+  {
+    if (state_ == BackendState::kRunning)
+      state_ = BackendState::kOpen;
+    else
+      throw BackendStateTransitionError();
+  }
+
+  void DummyBackend::write(const void* data, size_t bytes)
+  {
+    if (state_ == BackendState::kRunning)
+    {
+      if ( bytes > 0 )
+      {
+        std::this_thread::sleep_for( audio::bytesToUsecs(bytes, spec_) );
+      }
+    }
+    else
+      throw BackendStateTransitionError();
+  }
+
+  void DummyBackend::flush() {}
+
+  void DummyBackend::drain() {}
+
+  BackendState DummyBackend::state() const
+  {
+    return state_;
+  }
+
+}//namespace audio
---
+++ src/AudioBackendDummy.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2021 The GMetronome Team
+ * 
+ * This file is part of GMetronome.
+ *
+ * GMetronome is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GMetronome is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GMetronome.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GMetronome_AudioBackendDummy_h
+#define GMetronome_AudioBackendDummy_h
+
+#include "AudioBackend.h"
+
+namespace audio {
+
+  class DummyBackend : public AbstractAudioSink{
+
+  public:
+    DummyBackend(const audio::SampleSpec& spec = kDefaultSpec);
+    void configure(const SampleSpec& spec) override;
+    void open() override;
+    void close() override;
+    void start() override;
+    void stop() override;
+    void write(const void* data, size_t bytes) override;
+    void flush() override;
+    void drain() override;
+    BackendState state() const override;
+
+  private:
+    BackendState state_;
+    SampleSpec spec_;
+  };
+  
+}//namespace audio
+#endif//GMetronome_AudioBackendDummy_h
