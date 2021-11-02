ce47051e17afd229991421854e240cca7baa730f
dqpb <dqpb@mailbox.org>
2021-11-02 19:54:10 +0100

    Add Synthesizer

---
+++ src/Synthesizer.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2021 The GMetronome Team
+ * 
+ * This file is part of GMetronome.
+ *
+ * GMetronome is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GMetronome is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GMetronome.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GMetronome_Synthesizer_h
+#define GMetronome_Synthesizer_h
+
+#include "AudioBuffer.h"
+
+namespace audio {
+  
+  Buffer generateSound(double frequency,
+                       double volume,
+                       SampleSpec spec,
+                       microseconds duration);
+
+}//namespace audio
+#endif//GMetronome_Synthesizer_h
