19ad06339c087d48540440981b0fc99d645e9114
dqpb <dqpb@mailbox.org>
2021-12-03 02:23:09 +0100

    Add icons

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -246,7 +246,7 @@ void MainWindow::initAbout()
   //about_dialog_.set_website_label( about_dialog_.get_website() );
 
   about_dialog_.set_logo(
-    Gdk::Pixbuf::create_from_resource("/org/gmetronome/icons/gmetronome.svg",128,128));
+    Gdk::Pixbuf::create_from_resource("/org/gmetronome/icons/scalable/gmetronome.svg",128,128));
 
 }
 
